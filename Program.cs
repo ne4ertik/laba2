78ecda5e2976043288b41476a77be6b51901e4e4
Anya ฅ^•ﻌ•^ฅ <147582797+ne4ertik@users.noreply.github.com>
2023-10-23 07:48:23 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿
+Console.WriteLine("Введите три числа");
+double A=Convert.ToDouble(Console.ReadLine());
+double B = Convert.ToDouble(Console.ReadLine());
+double C = Convert.ToDouble(Console.ReadLine());
+if ((A < C) | (B < C) | (A < B)){
+   A=2*A; 
+   B=2*B;
+   C=2*C;
+    Console.WriteLine($"Удвоенные числа равны {A}{B}{C}") ;
+}
+ else
+{
+    Console.WriteLine("Ошибка");
+}
